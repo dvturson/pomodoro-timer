1847b1cb936915edaae73c9e9e452028d617ddc8
Devran Turson <dvturson@gmail.com>
2026-01-05 20:58:32 -0500

    Study and break time options where timer cycle between the two

--- script.js
+++ script.js
@@ -1,38 +1,66 @@
 console.log("Pomodoro!");
 
 let intervalId = null;
-let time_options = document.getElementById("time_options");
-let time_selection = parseInt(time_options.value);
-let count = time_selection;
-document.getElementById("timer").innerHTML = time_selection;
+let isStudying = true;
+let studyTimeSelect = document.getElementById("study_options");
+let breakTimeSelect = document.getElementById("break_options");
+let initialDuration = parseInt(studyTimeSelect.value) * 60;
+let timeLeft = initialDuration;
+reset();
 
 function countDown() {
-    if (count > 0) {
-        count --;
-        document.getElementById("timer").innerHTML = count;
-        console.log(count);
+    console.log(isStudying);
+    if (timeLeft > 0) {
+        timeLeft --;
+        document.getElementById("timer").innerHTML = formatTime();
+        console.log(timeLeft);
+    } else {
+        if (isStudying) {
+            isStudying = false;
+            timeLeft = parseInt(breakTimeSelect.value) * 60;
+        } else {
+             isStudying = true;
+            timeLeft = parseInt(studyTimeSelect.value) * 60;
+        }
     }
 }
 
 function reset() {
-    time_selection = parseInt(time_options.value);
-    count = time_selection;
-     document.getElementById("timer").innerHTML = count;
-    if (intervalId != null) {
+    initialDuration = parseInt(studyTimeSelect.value) * 60;
+    timeLeft = initialDuration;
+     document.getElementById("timer").innerHTML = formatTime();
+    if (intervalId !== null) {
         clearInterval(intervalId);
         intervalId = null;
     }
     console.log("reset")
 }
 
-time_options.addEventListener("change", () => {
+function formatTime() {
+    let minutes = parseInt(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    // console.log(minutes + ":" + seconds);
+    return minutes + ":" + seconds;
+}
+
+studyTimeSelect.addEventListener("change", () => {
+    reset();
+});
+breakTimeSelect.addEventListener("change", () => {
     reset();
 });
 
 const startButton = document.getElementById("startBtn");
 startButton.addEventListener("click", function() {
     if (intervalId === null) {
-        intervalId = setInterval(countDown, 1000);
+        intervalId = setInterval(countDown, 5);
     }  
 });
 
@@ -40,4 +68,3 @@ const resetButton = document.getElementById("resetBtn");
 resetButton.addEventListener("click", function() {
     reset();
 });
-
